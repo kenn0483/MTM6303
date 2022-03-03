a3c111d3c4116e2c79eacd0bc93a1b304bb09bfe
Eamonn Kennedy <kenn0483@algonquinlive.com>
2022-03-02 23:48:02 -0500

    Added the site folder

---
+++ inclass-4/site/includes/header.php
@@ -0,0 +1,41 @@
+<?php 
+// Assigning the value of session variable to the $title variable
+if(isset($_SESSION["page_title"])) {
+    $title = $_SESSION["page_title"]; 
+} else {
+    $title = "Inclass-4";
+}
+if(isset($_SESSION["current_page"])) {
+    $current_page = $_SESSION["current_page"]; 
+} else {
+    $current_page = "index";
+}
+
+?>
+  <title><?php echo $title; ?></title>  
+
+<header>
+  <nav class="navbar  navbar-fixed-top navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle uarr collapsed" data-toggle="collapse" data-target="#navbar-collapse-uarr">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="./index.php" title="">
+          <img src="./assets/images/mashuptemplate.svg" class="navbar-logo-img" alt="">
+        </a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="navbar-collapse-uarr">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="./index.php" title="" class="active"> Home</a></li>
+          <li><a href="./about.php" title=""> About</a></li>
+          <li><a href="./pricing.php" title=""> Pricing </a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
